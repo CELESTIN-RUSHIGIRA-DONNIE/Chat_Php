f163b56796501e3e528e56a7a7e20b6d78c08215
CELESTIN-RUSHIGIRA-DONNIE <celestinrushigiradonnie@gmail.com>
2024-03-21 15:28:33 +0100

    first commit

---
+++ chat.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Realtime Chat App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="img3.JPG" alt="">
+                <div class="details">
+                    <span>Donnie</span>
+                    <p>Active now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>e notre cerveau, ordinateur de notre </p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img3.JPG" alt="">
+                    <div class="details">
+                        <p> notre  ordinateur de notre blablablablablabala </p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>cerveau, ordinateur de notre </p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img3.JPG" alt="">
+                    <div class="details">
+                        <p>ordinateur de notre blablablablablabala </p>
+                    </div>
+                </div>
+            </div>
+            <form action="" method="POST" class="typing-area">
+                <input type="text" placeholder="Type a message here..">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+</body>
+
+</html>
